4942aff7fdc3aad4d09c0389e0bb2666544681a3
Anjumzaki <anjumzaki8@gmail.com>
2020-06-16 22:57:59 +0500

    sd

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI:
-      "mongodb+srv://aijaz:aijaz@cluster0-fgjhm.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
-      secretOrkey: "secret",
-  };
+  mongoURI: "mongodb://anjum:anjum123@ds161724.mlab.com:61724/ppass",
+  secretOrkey: "secret",
+};
